7b48b850591e010bd81f04f1efcbef0acd0bb236
Mahfoud-Sa <binsabbah2013@gmail.com>
2023-07-18 14:13:31 +0300

    add the login.php file

---
+++ logIn.php
@@ -0,0 +1,27 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'wantestdb';
+
+$usrName=$_GET['userName'];
+$usrPassword=$_GET['password'];
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+  echo($conn->connect_error);
+}
+$sql = "SELECT * FROM users WHERE name='$usrName' and password='$usrPassword'";
+
+$result = $conn->query($sql);
+
+
+echo json_encode($result->fetch_assoc());
+
+
+
+$conn->close();
+?>
